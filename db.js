39f80b0b2e687a9d2f126e308a11d2f4443521e5
MCiesiolka <mciesiolka@ivytech.edu>
2025-02-24 22:39:45 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://mciesiolka:Bat1724@songdb.j4d3d.mongodb.net/?retryWrites=true&w=majority&appName=songDB")
+
+module.exports = mongoose
+
