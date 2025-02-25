5e3e534c817e80081aef32caf479ba76f7db22d6
MCiesiolka <mciesiolka@ivytech.edu>
2025-02-25 08:39:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-  console.log(`Server is up and running on http://localhost:${PORT}`)
+  console.log(`Server is up and running on https://imported-humdrum-father.glitch.me/api/songs`)
 );
