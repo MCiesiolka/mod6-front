86cb04b07ef5ba87451baa4a75b516afeac2aa49
MCiesiolka <mciesiolka@ivytech.edu>
2025-02-24 23:39:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const Song = require("./songs");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
-
+app.use(cors({
+    origin: "https://mciesiolka.github.io/mod6-front/index.html",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type"
+}));
 app.use(express.json());
 
 const router = express.Router();
